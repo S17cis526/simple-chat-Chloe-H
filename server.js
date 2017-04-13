199969f6bc9833ee9b6206631c91ab3a6196bf94
Chloe <chenderson126+bitbucket@gmail.com>
2017-04-13 13:31:45 -0500

    Ran atom-beautify

--- server.js
+++ server.js
@@ -5,35 +5,35 @@ var http = require('http');
 var server = new http.Server(handleRequest);
 
 function handleRequest(req, res) {
-  switch(req.url) {
-    case '/':
-    case 'index.html':
-      fs.readFile('public/index.html', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/html");
-          res.end(data);
-        }
-      });
-      break;
-    case 'simple-chat.css':
-      fs.readFile('public/simple-chat', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/css");
-          res.end(data);
-        }
-      });
-      break;
-    case 'simple-chat.js':
-      fs.readFile('public/simple-chat.js', function(err, data){
-        if(err){
-          res.setHeader("Content-Type", "text/js");
-          res.end(data);
-        }
-      });
-      break;
-  }
+    switch (req.url) {
+        case '/':
+        case 'index.html':
+            fs.readFile('public/index.html', function(err, data) {
+                if (err) {
+                    res.setHeader("Content-Type", "text/html");
+                    res.end(data);
+                }
+            });
+            break;
+        case 'simple-chat.css':
+            fs.readFile('public/simple-chat', function(err, data) {
+                if (err) {
+                    res.setHeader("Content-Type", "text/css");
+                    res.end(data);
+                }
+            });
+            break;
+        case 'simple-chat.js':
+            fs.readFile('public/simple-chat.js', function(err, data) {
+                if (err) {
+                    res.setHeader("Content-Type", "text/js");
+                    res.end(data);
+                }
+            });
+            break;
+    }
 }
 
-server.listen(PORT, function(){
-  console.log(PORT);
+server.listen(PORT, function() {
+    console.log(PORT);
 });
